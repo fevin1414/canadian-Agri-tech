8427cfe1a5ca12447f3076401672e27f7fa3c041
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-18 01:47:38 -0500

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'support/index'
+  get 'about/index'
+  get 'cart/index'
+  get 'cart/update'
+  get 'cart/destroy'
+  get 'login/new'
+  get 'login/create'
+  get 'contact/index'
+  get 'contact/create'
+  get 'testimonials/index'
+  get 'products/index'
+  get 'products/show'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ test/controllers/cart_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class CartControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get cart_index_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get cart_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get cart_destroy_url
+    assert_response :success
+  end
+end
