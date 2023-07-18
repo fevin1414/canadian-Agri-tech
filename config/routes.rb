342ab8137543c1ccf7c2898f9f38279cd13b7488
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-18 02:35:15 -0500

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  get 'support/index'
-  get 'about/index'
-  get 'cart/index'
-  get 'cart/update'
-  get 'cart/destroy'
-  get 'login/new'
-  get 'login/create'
-  get 'contact/index'
-  get 'contact/create'
-  get 'testimonials/index'
-  get 'products/index'
-  get 'products/show'
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'services/index'
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'products', to: 'products#index'
+  get 'products/:id', to: 'products#show', as: 'product'
+
+  get 'testimonials', to: 'testimonials#index'
+
+  get 'contact', to: 'contact#index'
+  post 'contact', to: 'contact#create'
+
+  get 'signin', to: 'users#new'
+  post 'signin', to: 'users#create'
+
+  get 'cart', to: 'cart#index'
+  patch 'cart/:id', to: 'cart#update', as: 'update_cart'
+  delete 'cart/:id', to: 'cart#destroy', as: 'delete_cart'
+
+  get 'about', to: 'about#index'
+
+  get 'support', to: 'support#index'
 end
