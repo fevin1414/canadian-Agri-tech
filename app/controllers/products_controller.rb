7dd5abc82e9434106fd2135d20f50f41e1f66bed
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-20 02:36:55 -0500

    Search through the available products using a keyword search by category.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,24 @@
-
-
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
-    @products = Product.all
-    @products = Product.page(params[:page]).per(5)
+
+    # Check if a category is selected in the dropdown
+    if params[:category_id].present?
+      @category = Category.find(params[:category_id])
+      @products = @category.products
+    else
+      @products = Product.all
+    end
+
+    # Check if a keyword is provided for search
+    if params[:keyword].present?
+      keyword = "%#{params[:keyword].downcase}%"
+      @products = @products.where("lower(name) LIKE ? OR lower(description) LIKE ?", keyword, keyword)
+    end
+
+    @products = @products.page(params[:page]).per(5)
   end
+
 
   def show
     @product = Product.find(params[:id])
